5550f1b7a39ef36c483f86b2656943ac792e6e8d
aejmmark <aejmelaeus.markus@gmail.com>
2021-11-13 21:00:05 +0200

    1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -10,11 +10,21 @@ const Display = ({text, value}) => (
   <div>{text}{value}</div>
 )
 
+const Statistics = ({good, bad, neutral}) => (
+  <div>
+    <Display text="good " value={good} />
+      <Display text="neutral " value={neutral} />
+      <Display text="bad " value={bad} />
+      <Display text="all " value={good + bad + neutral} />
+      <Display text="average " value={(good - bad) / (good + bad + neutral)} />
+      <Display text="positive " value={good / (good + bad + neutral)} />
+  </div>
+)
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -22,13 +32,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <Display text="good " value={good} />
-      <Display text="neutral " value={neutral} />
-      <Display text="bad " value={bad} />
-
-      <Display text="all " value={good + bad + neutral} />
-      <Display text="average " value={(good - bad) / (good + bad + neutral)} />
-      <Display text="positive " value={good / (good + bad + neutral)} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
