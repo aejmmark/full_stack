f9f46f15c592165e12d15bf66e51f0ce0e84501e
aejmmark <aejmelaeus.markus@gmail.com>
2021-11-28 22:06:27 +0200

    2.2

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -5,6 +5,7 @@ const Course = ({course}) => {
     <>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
@@ -31,6 +32,16 @@ const Part = (props) => {
       <p>
         {props.part.name} {props.part.exercises}
       </p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  let sum = props.parts.reduce(
+    (sum, part) => sum + part.exercises, 0)
+  return (
+    <>
+      <p>Number of exercises {sum}</p>
     </>
   )
 }
