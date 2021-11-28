3cf3994f067494df5d78cba49210976922201193
aejmmark <aejmelaeus.markus@gmail.com>
2021-11-28 21:52:38 +0200

    2.1

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.map(part => <Part part={part} />)}
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
