df4cb7ebd93878bd7d14da65877e2c02d73f5314
aejmmark <aejmelaeus.markus@gmail.com>
2022-01-01 16:02:08 +0200

    3.12

---
+++ osa3/puhelinluettelo/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://aejmmark:${password}@cluster0.uwutp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  })
+  
+const Person = mongoose.model('Person', personSchema)
+  
+
+if (process.argv.length === 5) {
+    const personName = process.argv[3]
+    const personNumber = process.argv[4]
+      
+    const note = new Person({
+      name: personName,
+      number: personNumber,
+    })
+      
+    note.save().then(response => {
+      console.log(`added ${personName} number ${personNumber} to phonebook`)
+      mongoose.connection.close()
+    })
+} else if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+}
