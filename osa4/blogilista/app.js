e5330252b8871960f4b694502576ac4f04dcead5
aejmmark <aejmelaeus.markus@gmail.com>
2022-01-04 15:10:55 +0200

    4.1 - 4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+//app.use(express.static('build'))
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
