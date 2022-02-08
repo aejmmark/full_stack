94d1542cf8c2d0e26f1951010f55ca6745ba085a
aejmmark <aejmelaeus.markus@gmail.com>
2022-02-08 23:30:14 +0200

    6.13 - 6.14

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
 import AnecdoteForm from './components/anecdoteForm'
 import AnecdoteList from './components/anecdoteList'
 import Notification from './components/notification'
 import Filter from './components/filter'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+    }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- osa6/redux-anecdotes/src/components/anecdoteForm.js
+++ osa6/redux-anecdotes/src/components/anecdoteForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import anecdoteService from '../services/anecdotes'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 import { removeNotification, addNotification } from '../reducers/notificationReducer'
@@ -6,11 +7,12 @@ import { removeNotification, addNotification } from '../reducers/notificationRed
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(addNotification(`${content} added`))
     setTimeout(() => {
       dispatch(removeNotification())
